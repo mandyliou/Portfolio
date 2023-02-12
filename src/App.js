6e8ccb954af6c1db14ec573c579045fa4177a7df
Mandy Liou <mandyliou@icloud.com>
2023-02-12 12:07:19 -0800

    changed app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
@@ -36,7 +36,7 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route exact path="/portfolio/" element={<Home />} />
+          <Route exact path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<Navigate to="/"/>} />
